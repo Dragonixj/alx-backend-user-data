9f4f6f1b0ec355d322eeee93413eb2d0aa1d571d
Dragonixj <delafiado@gmail.com>
2024-05-19 19:10:30 +0000

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,4 +9,5 @@ def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
     """Returns the log message obfuscated"""
-    ...
+    keys = "=|".join(fields)
+    return re.sub(rf"({keys}=)[^{separator}]*", rf"\1{redaction}", message)
