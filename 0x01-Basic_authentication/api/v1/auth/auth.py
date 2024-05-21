9f6f3822fa4c8684f41a9fbae1e1665f876d9b4f
Dragonixj <delafiado@gmail.com>
2024-05-21 13:34:25 +0000

    Task 5
    - restructure

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,14 +10,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if auth is required"""
-        if path not in excluded_paths:
+        if path is None or not excluded_paths:
             return True
         path = path if path.endswith("/") else path + "/"
         return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Extract the authorization header"""
-        return "None"
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar("User"):
         """Get the current user"""
