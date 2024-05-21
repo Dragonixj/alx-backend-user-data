e20df6ae487765e9843bd6d5d5c939c069d9aa6d
Dragonixj <delafiado@gmail.com>
2024-05-21 12:40:16 +0000

    :Task 0

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,9 +5,10 @@ Route module for the API
 import os
 from os import getenv
 
-from api.v1.views import app_views
 from flask import Flask, abort, jsonify, request
 from flask_cors import CORS, cross_origin
+
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
