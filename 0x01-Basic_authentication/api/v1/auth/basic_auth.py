6c28111e60d41ac0e41738655696a08500509ffd
Dragonixj <delafiado@gmail.com>
2024-05-21 22:27:26 +0000

    Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 """Basic Auth"""
 
 import base64
-from typing import TypeVar
+
+from models.user import User
 
 from .auth import Auth
 
@@ -47,3 +48,21 @@ class BasicAuth(Auth):
             return None, None
         user_email, user_pwd = decoded.split(":", 1)
         return user_email, user_pwd
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> "User":
+        """User Object From Credentials"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
