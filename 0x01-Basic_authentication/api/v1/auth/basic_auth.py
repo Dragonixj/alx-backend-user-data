364ee15d40ed08187bb28ea5df0ca7d3cd86e978
Dragonixj <delafiado@gmail.com>
2024-05-21 14:42:06 +0000

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -19,3 +19,17 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header.lstrip("Basic ")
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Decode the value of base64 in the Authorization header"""
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return None
+        try:
+            decoded_header = base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
+        return decoded_header.decode()
