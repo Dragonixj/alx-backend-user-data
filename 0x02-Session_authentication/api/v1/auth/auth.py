0e00b2cdb426dece8eead7ac7785d4ab33ee0aea
Dragonixj <delafiado@gmail.com>
2024-05-24 12:37:21 +0000

    Task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Authentication template"""
+from typing import List, TypeVar
+
+from flask import request
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if auth is required"""
+        if path is None or not excluded_paths:
+            return True
+        path = path if path.endswith("/") else path + "/"
+        return path not in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """Extract the authorization header"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Get the current user"""
+        return None
