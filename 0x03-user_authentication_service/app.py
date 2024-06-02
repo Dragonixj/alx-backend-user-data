16b0f953f9c3e112aaea325c830d617ba36bd74c
Dragonixj <delafiado@gmail.com>
2024-06-02 21:48:38 +0000

    task 11 & 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -25,7 +25,42 @@ def users():
         AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
-        return jsonify({"message": "email already registered"})
+        return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Log a user in and crete a session"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if not session_id:
+            abort(401)
+        response = make_response(jsonify({"email": email, "message": "logged in"}))
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """
+    Logs User out and deletes Session
+    """
+
+    session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for("welcome"))
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
